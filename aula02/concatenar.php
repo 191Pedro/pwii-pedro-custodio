b241e0e76da7f89d2c6c7a30a306a28a3ceb6b23
Pedro Henrique Custódio <pedropedroka130608@gmail.com>
2025-02-27 07:33:08 -0300

    Aula01

---
+++ aula02/concatenar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="cor">
+            <?php
+                ECHO "Hello World!<br>";
+                echo "Hello World!<br>";
+                EcHo "Hello World!<br>";
+
+                $nome = "Carol";
+                $sobrenome = "Candela";
+                $numero = 9;
+
+                echo $nome . " linda";
+                echo "${nome} <h1> ${sobrenome} </h1>";
+                echo $numero . "<p> " . $nome . " " . $sobrenome . "</p>";
+            ?>
+        </div>
+    </body>
+</html>
+<link href="../aula02/concatenar.css" type="text/css"  rel="stylesheet">
