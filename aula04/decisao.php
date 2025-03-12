c036774f337cc1bae56b4ccc1941b28bf1076676
Pedro Henrique Custódio <pedropedroka130608@gmail.com>
2025-03-12 10:38:35 -0300

    Aula04

---
+++ aula04/decisao.php
@@ -0,0 +1,48 @@
+<?php
+
+    $n = 6;
+
+    if ($n >= 4) {
+        echo $n, " é maio ou igual a 4 + uma alegação quase plausível";
+    };
+
+    $n1 = 3;
+    $n2 = 4;
+
+    $result = ($n1%$n2)%2;
+
+    if ($result == 1) {
+        echo "<br>", $result, "quociente ímpar de novo";
+    }
+    else{
+        echo "<br>", $result, "quociente de par";
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
